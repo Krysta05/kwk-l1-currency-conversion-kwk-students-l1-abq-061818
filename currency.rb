67bd4412131809164947ed675380e514db8262d5
Krysta05 <martinezkrysta7@gmail.com>
2018-06-22 15:02:13 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,3 +1,7 @@
-usd_to_eu = ("u.s. dollors" "euros dollors")
-eur_to_usd = ("euros dollors" "u.s. dollors")
+puts "How much money do yo want to convert?"
+amount = gets.chomp().to_i 
 
+puts "Pick from the following menu:"
+puts 1. "USD to EUR"
+puts "USD to JPY"
+puts ""
